7294f4904311495cab6c6e19c441c3b7fdd2d4b3
Kayla Johnson <kayla.elizabeth89@gmail.com>
2020-12-20 23:22:11 -0800

    Hardcoded data in app now printing for wave 1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import timelineData from './data/timeline.json';
 import Timeline from './components/Timeline';
+import TimelineEvent from './components/TimelineEvent';
+
+// const timeLineDatas = [
+//   {
+//     person: 'Adele Goldberg',
+//     status: 'In Smalltalk, everything happens somewhere else.',
+//     timeStamp: '2018-05-18T22:12:03Z',
+
+//   },
+  
+// ]
 
 function App() {
   console.log(timelineData);
@@ -14,9 +25,11 @@ function App() {
         <h1 className="App-title">Ada Lovelace's social media feed</h1>
       </header>
       <main className="App-main">
+        <TimelineEvent person="Adele Goldberg" status="In Smalltalk, everything happens somewhere else." timeStamp= "2018-05-18T22:12:03Z"
+        />
       </main>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,20 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
+const TimelineEvent = (props) => {
   
-  return;
-}
+  return (
+    <div>
+      <ul>
+      <li>{props.person}</li>
+      <li>{props.status}</li>
+      <li><Timestamp time={props.timestamp} /></li>
+      </ul>
+    </div>
+
+
+
+  );
+};
 
 export default TimelineEvent;
